4962adf896ceb5066edefef46e5eb11d165b191f
Marek Antoniak <mantoniak@kplabs.pl>
2020-11-18 10:13:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reed-Solomon
+
+Reed Solomon (255, 223) CCSDS in Python.
+
+Library based on https://github.com/crozone/ReedSolomonCCSDS
---
+++ reed_solomon_ccsds/__init__.py
@@ -0,0 +1,3 @@
+from .reed_solomon import UncorrectableError, decode, decode_block, encode, encode_block
+
+__all__ = ['UncorrectableError', 'decode', 'decode_block', 'encode', 'encode_block']
